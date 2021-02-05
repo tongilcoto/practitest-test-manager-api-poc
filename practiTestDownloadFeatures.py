58b96c11dc0df94306818fdfc001cfd8a7e5238b
tongilcoto <tongilcoto@gmail.com>
2021-02-05 20:14:08 +0100

    Last final touches at the script

--- practiTestDownloadFeatures.py
+++ practiTestDownloadFeatures.py
@@ -4,16 +4,16 @@ import requests
 from jsonpath_ng.ext import parse
 
 
-def get_instances_ids_of_suite(projectId, testSetId):
+def get_instances_ids_of_suite(project_id, test_set_id):
     """
 
-    :param projectId:
-    :param testSetId:
+    :param project_id:
+    :param test_set_id:
     :return:
     """
     # curl -H "Content-Type: application/json" -H "PTToken: $TOKEN" https://api.practitest.com/api/v2/projects/18098/tests.json
-    url = os.environ['BASEURL_practitest'] + os.environ['INSTANCESPATH_practitest'].format(projectId=projectId,
-                                                                                           testSetId=testSetId)
+    url = os.environ['BASEURL_practitest'] + os.environ['INSTANCESPATH_practitest'].format(projectId=project_id,
+                                                                                           testSetId=test_set_id)
     print(url)
     response = requests.get(url,
                             headers={
@@ -25,30 +25,6 @@ def get_instances_ids_of_suite(projectId, testSetId):
     print(response.json())
 
     return response.json()
-
-
-def get_feature_definition(projectId, testId):
-    """
-
-    :param projectId:
-    :param testId:
-    :return:
-    """
-    url = os.environ['BASEURL_practitest'] + os.environ['TESTPATH_practitest'].format(projectId=projectId,
-                                                                                      testId=testId)
-    print(url)
-    response = requests.get(url,
-                            headers={
-                                'accept': 'application/json',
-                                'PTToken': os.environ['TOKEN_practitest']
-                            })
-    print(response)
-    print(response.json())
-    attributes = response.json()['data']['attributes']
-    examples = attributes['custom-fields'].get(os.environ['EXAMPLESCUSTOMFIELD_practitest'], '')
-    return attributes['name'], \
-           attributes['description'], \
-           examples
 
 
 def get_steps(project_id, test_ids):
@@ -106,7 +82,6 @@ def create_feature_files(projectId, instances_ids, test_display_ids):
     :param test_display_ids:
     :return:
     """
-
     test_display_ids_list = ','.join(map(str, test_display_ids))
     url = os.environ['BASEURL_practitest'] + os.environ['TESTSPATH_practitest']. \
         format(projectId=projectId,
